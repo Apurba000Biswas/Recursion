39914d83586141c273bc46f24cc2d3bb8337588b
Apurba <apurba10024@gmail.com>
2019-03-09 17:27:10 +0600

    1.0 Added All files

---
+++ src/practise_set_1/RecursionProb.java
@@ -0,0 +1,13 @@
+package practise_set_1;
+
+public class RecursionProb {
+	
+	public static void main(String[] args){
+		RecursionProb obj = new RecursionProb();
+		obj.run();
+	}
+	
+	private void run(){
+		
+	}
+}
