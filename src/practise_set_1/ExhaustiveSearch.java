726d46b0ffbd11785d3576007159b1643aa51fed
Apurba <apurba10024@gmail.com>
2019-03-14 15:25:41 +0600

    4.4 Added Exhaustive Search Problems

---
+++ src/practise_set_1/ExhaustiveSearch.java
@@ -0,0 +1,78 @@
+package practise_set_1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExhaustiveSearch {
+	
+	
+	public static void main(String[] args){
+		ExhaustiveSearch obj = new ExhaustiveSearch();
+		obj.run();
+	}
+	
+	private void run(){
+		System.out.println("printBinary(" + 4 + ")");
+		printBinary(4);
+		
+		System.out.println("diceRoll(3,7)");
+		diceRoll(3,7);
+	}
+	/*printBinary(2)
+	 * 00
+	 * 01
+	 * 10
+	 * 11
+	 * printBinary(3)
+	 * 000
+	 * 001
+	 * 010
+	 * 011
+	 * 100
+	 * 101
+	 * 110
+	 * 111
+	 */
+	private void printBinary(int digits){
+		StringBuilder sbOutput = new StringBuilder();
+		printAllBinaryHelper(digits, sbOutput);
+	}
+	
+	private void printAllBinaryHelper(int digits, StringBuilder output){
+		if(digits == 0){
+			// base case nothing to do
+			System.out.println(output.toString());
+		}else{
+			// recursive case
+			String thisOutput = output.toString();
+			printAllBinaryHelper(digits-1, output.append(0));
+			output = new StringBuilder(thisOutput);
+			printAllBinaryHelper(digits-1, output.append(1));
+		}
+	}
+	
+	
+	
+	private void diceRoll(int diceNum, int diceSum){
+		List<Integer> chosen = new ArrayList<>();
+		diceRollHelper(diceNum, diceSum, chosen);
+	}
+	
+	private void diceRollHelper(int diceNum, int diceSum, List<Integer> chosen){
+		if(diceNum == 0){
+			// base case - no dice to roll
+			if(diceSum == 0){
+				System.out.println(chosen);
+			}
+		}else{
+			// for each rolling 1-6
+			for(int i=1; i<=6; i++){
+				chosen.add(i);
+				diceRollHelper(diceNum-1, diceSum-i, chosen);
+				
+				int lastIndex = chosen.size()-1;
+				chosen.remove(lastIndex);
+			}
+		}
+	}
+}
